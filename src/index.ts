bc3c67baaff716be708341191de79a940907e91c
anyifan <914956648@qq.com>
2024-11-08 12:04:28 +0800

    first commit

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,14 @@ export const Config: Schema<Config> = Schema.object({})
 
 export function apply(ctx: Context) {
   // write your plugin here
+
+  ctx.middleware( (session, next) => {
+    // do something
+    if (session.content === "开黑"){
+      return "开黑啦"
+    } else{
+      return next()
+    }
+
+  })
 }
